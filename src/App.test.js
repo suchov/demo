a87b82e12c7d95ca6cbd26afe321e2e785ba9ef2
suchov <artem.suchov@gmail.com>
2018-11-13 15:21:41 +0200

    play with wrapper nad jest toBe and expect

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, {shallow} from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
+Enzyme.configure({adapter: new EnzymeAdapter()});
+
 test('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = shallow(<App />);
+  expect(wrapper).toBeTruthy();
+  //https://jestjs.io/docs/en/expect#tobevalue
+  //https://jestjs.io/docs/en/expect#tobetruthy
+  // https://jestjs.io/docs/en/expect#expectnotstringcontainingstring
+  // https://jestjs.io/docs/en/expect#expectobjectcontainingobject
+  // console.log(wrapper.debug());
+});
+
+test('some fucking testing', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toBeTruthy();
 });
